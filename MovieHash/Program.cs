664f39f91947a86a6120ec30733f08914ee177e4
Johann <jwjosefy@gmail.com>
2015-02-21 00:52:41 -0200

    Move hash functions to class Hashing

--- MovieHash/Program.cs
+++ MovieHash/Program.cs
@@ -12,53 +12,6 @@ namespace MovieHash
     {
         const string URLBASE = @"http://www.opensubtitles.org/pb/search/sublanguageid-pob/moviehash-";
 
-        private static byte[] ComputeMovieHash(string filename)
-        {
-            byte[] result;
-            using (Stream input = File.OpenRead(filename))
-            {
-                result = ComputeMovieHash(input);
-            }
-            return result;
-        }
-
-        private static byte[] ComputeMovieHash(Stream input)
-        {
-            long lhash, streamsize;
-            streamsize = input.Length;
-            lhash = streamsize;
-
-            long i = 0;
-            byte[] buffer = new byte[sizeof(long)];
-            while (i < 65536 / sizeof(long) && (input.Read(buffer, 0, sizeof(long)) > 0))
-            {
-                i++;
-                lhash += BitConverter.ToInt64(buffer, 0);
-            }
-
-            input.Position = Math.Max(0, streamsize - 65536);
-            i = 0;
-            while (i < 65536 / sizeof(long) && (input.Read(buffer, 0, sizeof(long)) > 0))
-            {
-                i++;
-                lhash += BitConverter.ToInt64(buffer, 0);
-            }
-            input.Close();
-            byte[] result = BitConverter.GetBytes(lhash);
-            Array.Reverse(result);
-            return result;
-        }
-
-        private static string ToHexadecimal(byte[] bytes)
-        {
-            StringBuilder hexBuilder = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                hexBuilder.Append(bytes[i].ToString("x2"));
-            }
-            return hexBuilder.ToString();
-        }
-
         static void Main(string[] args)
         {
             if (!args.Any())
@@ -77,7 +30,7 @@ namespace MovieHash
                 path = Path.Combine(Directory.GetCurrentDirectory(), args[0]);
             }
 
-            var hash = ToHexadecimal(ComputeMovieHash(path));
+            var hash = Hashing.ToHexadecimal(Hashing.ComputeMovieHash(path));
             Console.WriteLine("OpenSubtitles Hash ==>  {0}", hash);
 
             var url = URLBASE + hash;
