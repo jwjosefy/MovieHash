825edacb68ea02c5b985ce16060619f0e103f404
jwjosefy <jwjosefy@gmail.com>
2015-02-23 14:32:01 -0300

    Subtitle automatic download - GetSubtitle: navigates to OS, gets srt link, download zip, read srt file - Unit Test (initial)

---
+++ MovieHash.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieHash.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        const string URLBASE = @"http://www.opensubtitles.org/pb/search/sublanguageid-pob/moviehash-";
+
+
+        [TestMethod]
+        public void TestLoadSubtitleFromHash()
+        {
+            var hash = "05f5f4ed29b1aa4e";
+
+            MovieHash.Program.GetSubtitle(URLBASE + hash);
+            Assert.Inconclusive();
+        }
+    }
+}
--- MovieHash/Program.cs
+++ MovieHash/Program.cs
@@ -5,10 +5,13 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Compression;
+using RestSharp;
+using CsQuery;
 
 namespace MovieHash
 {
-    class Program
+    public class Program
     {
         const string URLBASE = @"http://www.opensubtitles.org/pb/search/sublanguageid-pob/moviehash-";
 
@@ -35,6 +38,49 @@ namespace MovieHash
 
             var url = URLBASE + hash;
             Process.Start(url);
+        }
+
+        public static void GetSubtitle(string url)
+        {
+            const string BASE_DOWNLOAD = "http://dl.opensubtitles.org/pb/download/sub/";
+
+            var page = CQ.CreateFromUrl(url);   // Loads OpenSubtitle page with download link
+
+            var links = page["a[href*='subtitleserve']"];   // Find download links
+
+            if (links.Any())
+            {
+                // Extract file id and download count
+                var subs = links.Select(e => new 
+                { 
+                    id = e.GetAttribute("href").Split('/').Last(),
+                    count = Convert.ToInt32(e.InnerText.Replace("x", ""))
+                });
+
+                // Get the one most downloaded
+                var sub = subs.FirstOrDefault(e => e.count == subs.Max(e2 => e2.count));
+
+                // Download the zip
+                var cli = new RestClient(BASE_DOWNLOAD + sub.id);
+                var resp = cli.Get(new RestRequest());
+
+                // Read as zip file
+                using (var mem = new MemoryStream(resp.RawBytes))
+                {
+                    using (var zip = new ZipArchive(mem, ZipArchiveMode.Read))
+                    {
+                        Debug.WriteLine("Files: {0}", zip.Entries.Count);
+                        foreach (var item in zip.Entries)
+                        {
+                            if (item.Name.EndsWith(".srt"))
+                            {
+                                // Found the subtitle                                
+                                var srt = new StreamReader(item.Open()).ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }            
         }
     }
 }
